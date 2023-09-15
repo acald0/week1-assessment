5a326bc8cd6ca7c9db6108e1e27a556f14d5272d
acald0 <annecaldwell2002@gmail.com>
2023-09-15 11:34:17 -0600

    finished sdlc

--- sdlc.js
+++ sdlc.js
@@ -12,6 +12,14 @@
 // Testing & Integration
 // Planning
 
+myOrder:
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +27,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, in which the developer decides what the software needs to do. An example of what those requirements could be is deciding the software needs to have a login and a logout."
+const design = "This step consists of making decisions on how you're going to meet the requirements outlined in the previous step. Not only does this step determine visual design, but also the coding language used to build the software, what the user interface will be like, and more."
+const implementation = "This step evolves around actually coding the design determined in the previous step. This step should also include testing throughout."
+const testingAndIntegration = "When coding is finished, there is final testing of requirements. The software is then inegrated to make sure the users can actually use it (e.x. publishing the website or launching the app)."
+const maintain = "This step is maintenance on the software after it's been published. This includes making sure the app continues to work as it is updated or adding new features to a website."
